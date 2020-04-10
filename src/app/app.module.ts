3ea4262a6dc885a4e0c97ced10473233f2e259d3
TERRYYYY <kariuki.terry11@gmail.com>
2020-04-10 10:05:50 +0300

    add component that generates the 404 page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { GithubComponent } from './github/github.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RepoSearchComponent } from './repo-search/repo-search.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GithubComponent,
     NavbarComponent,
-    RepoSearchComponent
+    RepoSearchComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
