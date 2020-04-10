5c53c488ebe681326bbba3c2e99deb429ac8c1aa
TERRYYYY <kariuki.terry11@gmail.com>
2020-04-10 16:41:43 +0300

    added personal access token

--- src/app/classes/user.ts
+++ src/app/classes/user.ts
@@ -1,2 +1,12 @@
 export class User {
+    constructor(
+        public name: string,
+        public avatar_url: string,
+        public location: string,
+        public bio: string,
+        public followers: number,
+        public following: number,
+    ){
+
+    }
 }
