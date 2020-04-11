ad0ea93f7faa3c8d6d8b5141c402e8c23a4377d9
TERRYYYY <kariuki.terry11@gmail.com>
2020-04-11 14:13:51 +0300

    added http client

--- src/app/github/github.component.ts
+++ src/app/github/github.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient } from '@angular/common/http';
+import { User } from '../classes/user';
 
 @Component({
   selector: 'app-github',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./github.component.css']
 })
 export class GithubComponent implements OnInit {
+  user:User;
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
-  }
+    interface ApiResponse{
+      name: string;
+    avatar_url: string;
+    location: string;
+    bio: string;
+    followers: number;
+    following: number;
+    }
+    this.http.get<ApiResponse>("e26d816eac22b17ab727ee8bd10a66dbebad1e51").subscribe(data=>{
+      this.user =new User (data.name,data.avatar_url,data.bio,data.location,data.followers,data.following)
+    })
 
+}
 }
