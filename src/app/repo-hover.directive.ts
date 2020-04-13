ed3247120eb0fca390cd56900fdef6ea7fb66bc5
TERRYYYY <kariuki.terry11@gmail.com>
2020-04-13 14:05:09 +0300

    added a hover effect on the repositories using a custom directive

--- src/app/repo-hover.directive.ts
+++ src/app/repo-hover.directive.ts
@@ -1,10 +1,20 @@
-import { Directive } from '@angular/core';
+import { Directive,ElementRef,HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appRepoHover]'
 })
 export class RepoHoverDirective {
 
-  constructor() { }
+  constructor(private elem: ElementRef) { }
+  @HostListener("mouseenter") onMouseEnter() {
+    this.overlay("rgba(0,0,0,0.1)");
+  }
+
+  @HostListener("mouseleave") onMouseLeave() {
+    this.overlay(null);
+  }
+  private overlay(color: any) {
+    this.elem.nativeElement.style.backgroundColor = color;
+  }
 
 }
